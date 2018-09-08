bf01d3be814b30fa668df34237f36b8ba339fd75
Adroso <adrian.lapico@my.jcu.edu.au>
2018-09-08 22:23:05 +1000

    starting the work on handeling inversing

---
+++ configs.py
@@ -0,0 +1,25 @@
+#Kyalls
+#THE MAIN ONE
+PIXEL_VALUE_TO_ACTUAL_VALUE_FACTOR = 3
+
+#For pixel counting Algorithim
+PIXEL_TO_LOOK = 1
+INVERSE_PIXEL_TO_LOOK = 0
+DISTANCE_THRESHOLD = 40
+PIXEL_IGNORE_THRESHOLD = 30
+
+#For Lens Correction
+cam_maker = 'GoPro'
+cam_model = 'HERO4 Silver'
+lens_maker = 'GoPro'
+lens_model = 'HERO4'
+focal_length = 5
+apperture = 2.97
+
+#For Cropping
+left_crop = 860
+right_crop = 1600
+top_crop = 50
+bottom_crop = 2900
+NUMBER_OF_OYSTERS_HIGH = 8
+NUMBER_OF_OYSTERS_WIDE = 2
