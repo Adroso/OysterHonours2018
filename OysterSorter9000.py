52073450522c28f0d8c85e02172b9075081baa5f
Adroso <adrian.lapico@my.jcu.edu.au>
2018-09-18 16:53:03 +1000

    created the sorter

--- OysterSorter9000.py
+++ OysterSorter9000.py
@@ -11,30 +11,37 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-PATH_TO_IMAGES = "F:\\Honours Image Library\\Oysters\\archive\\3x3\\"
-CSV = "3x3csv.csv"
-CSV_NEW = "keptOysters.csv"
+PATH_TO_IMAGES = "E:\\Oysters\\canny\\"
+CSV = "canny.csv"
+CSV_NEW = "keptOystersC.csv"
 
 input_csv = open(CSV, 'r')
 output_csv = open(CSV_NEW, 'w')
 
 i = 0
+no_oyster = 0
 for line in input_csv:
-    if i !=0:
+    if i > 0:
         line = line.split(',')
         image_to_process = PATH_TO_IMAGES+line[0]+".JPG"
-        raw_image = cv2.imread(image_to_process)
-        plt.imshow(raw_image)
-        plt.show()
+        try:
+            raw_image = cv2.imread(image_to_process)
+            plt.imshow(raw_image)
+            plt.show()
+        except:
+            print(line[0])
+            pass
         user_input = input("Keep?")
         if user_input == "1":
             output_csv.write(line[0]+ "," +line[1]+","+line[2])
+        elif user_input =="4": #no oyster
+            no_oyster += 1
         else:
             pass
-
         if user_input == "9":
             break
     i+=1
 
+print(no_oyster)
 input_csv.close()
 output_csv.close()
